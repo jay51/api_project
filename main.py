74c7b845f06eb5eac6b2fa6702fc62eebea4e685
jay51 <nenojafaar@gmail.com>
2023-07-04 19:44:45 -0500

    add new comments

--- main.py
+++ main.py
@@ -5,3 +5,5 @@
 # git init to create a git repo
 # git status to check the status of the repo
 # git add <file> add the file to the staging space
+# git log show a list of the commits
+# git diff display the changes in the file
